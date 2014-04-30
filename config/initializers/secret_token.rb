0302f2f8f722f27dc471dfd2e948851ea21f3085
Essam Joubori <essam.joubori@gmail.com>
2014-04-30 10:00:32 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scrolling::Application.config.secret_key_base = '956f35fd3a461f85e5090c88d9cbb428eb34cb6e28d685d8c47823691a86950f4d538f086971682473f533f0f6c3a8d563ba485a5f5ce16b91b393a26969865c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scrolling::Application.config.session_store :cookie_store, key: '_scrolling_session'
